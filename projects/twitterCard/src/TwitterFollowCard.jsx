3a737b2c7da8f47d0279dd2cc79e19ef56ded94b
Gian Barboza <gbarboza@hablax.com>
2023-01-29 12:48:40 -0500

    Add good practice in REACT

--- projects/twitterCard/src/TwitterFollowCard.jsx
+++ projects/twitterCard/src/TwitterFollowCard.jsx
@@ -1,5 +1,22 @@
-export function TwitterFollowCard ({ name, userName, imageUrl, isFollowing }) {
-  console.log(isFollowing)
+import { useState } from 'react'
+
+export function TwitterFollowCard ({ children, userName = 'Undefined', imageUrl, initialIsFollowing }) {
+  // Usando destructuracion
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)// El useState tiene un arreglo donde el primero es el estado y el segundo es donde se cambia ese estado
+  // Sin usar destructuracion como se ve se puede hacer esto en una sola linea destructurando el array que retorna el useState
+  // const isFollowingState = useState(false)
+  // const isFollowing = isFollowingState[0]
+  // const setIsFollowing = isFollowingState[1]
+
+  // Logica para cambiar el estilo del boton dependiendo el estado
+  const isFollowingStateText = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassNameStyle = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+  // Funcion para cambiar el estado de isFollowing
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
   return (
     <article className='tw-followCard'>
       <header className='tw-followCard-header'>
@@ -9,12 +26,15 @@ export function TwitterFollowCard ({ name, userName, imageUrl, isFollowing }) {
           alt='El avatart de unavatar.io'
         />
         <div className='tw-followCard-info'>
-          <strong>{name}</strong>
+          <strong>{children}</strong>
           <span className='tw-followCard-infoUserName'>@{userName}</span>
         </div>
       </header>
       <aside>
-        <button className='tw-followCard-button'>Seguir</button>
+        <button className={buttonClassNameStyle} onClick={handleClick}>
+          <span className='tw-followCard-follow'>{isFollowingStateText}</span>
+          <span className='tw-followCard-unFollow'>Dejar de Seguir</span>
+        </button>
       </aside>
     </article>
   )
